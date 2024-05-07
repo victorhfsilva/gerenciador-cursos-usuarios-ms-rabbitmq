7ce1a81c0b822aaef90b82a6b35e5d1ecf493c6b
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-05-07 19:29:43 -0300

    test: Adição de testes do controladores de usuários.

--- src/test/java/com/example/usuariosms/controller/UsuarioControllerTest.java
+++ src/test/java/com/example/usuariosms/controller/UsuarioControllerTest.java
@@ -1,10 +1,13 @@
 package com.example.usuariosms.controller;
 
 import com.example.usuariosms.fixture.UsuarioFixture;
+import com.example.usuariosms.fixture.UsuarioRequestFixture;
 import com.example.usuariosms.fixture.UsuarioResourceFixture;
 import com.example.usuariosms.model.Usuario;
+import com.example.usuariosms.model.dto.UsuarioRequest;
 import com.example.usuariosms.model.resources.UsuarioResource;
 import com.example.usuariosms.service.impl.UsuarioService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -76,6 +79,72 @@ public class UsuarioControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.usuarioResourceList.[0].dataNascimento").value(usuarioResources.get(0).getDataNascimento().toString()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.usuarioResourceList.[0].email").value(usuarioResources.get(0).getEmail()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.usuarioResourceList.[0].celular").value(usuarioResources.get(0).getCelular()));
+    }
+
+
+    @Test
+    void registrarUsuarioTest() throws Exception {
+        UsuarioResource usuarioResource = UsuarioResourceFixture.buildValido();
+        UsuarioRequest usuarioRequest = UsuarioRequestFixture.buildValido();
+
+        String usuarioRequestJson =  objectMapper.writeValueAsString(usuarioRequest);
+
+        when(usuarioService.save(any(UsuarioRequest.class))).thenReturn(usuarioResource);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/usuarios")
+                        .contentType("application/json")
+                        .content(usuarioRequestJson))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(usuarioResource.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value(usuarioResource.getNome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sobrenome").value(usuarioResource.getSobrenome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value(usuarioResource.getCpf()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.papel").value(usuarioResource.getPapel().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dataNascimento").value(usuarioResource.getDataNascimento().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(usuarioResource.getEmail()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.celular").value(usuarioResource.getCelular()));
+    }
+
+    @Test
+    void atualizarUsuarioTest() throws Exception {
+        UsuarioResource usuarioResource = UsuarioResourceFixture.buildValido();
+        UsuarioRequest usuarioRequest = UsuarioRequestFixture.buildValido();
+
+        String usuarioRequestJson =  objectMapper.writeValueAsString(usuarioRequest);
+
+        when(usuarioService.update(any(UUID.class), any(UsuarioRequest.class))).thenReturn(usuarioResource);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/usuarios/{id}", UUID.randomUUID())
+                        .contentType("application/json")
+                        .content(usuarioRequestJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(usuarioResource.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value(usuarioResource.getNome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sobrenome").value(usuarioResource.getSobrenome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value(usuarioResource.getCpf()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.papel").value(usuarioResource.getPapel().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dataNascimento").value(usuarioResource.getDataNascimento().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(usuarioResource.getEmail()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.celular").value(usuarioResource.getCelular()));
+    }
+
+    @Test
+    void deletarUsuarioTest() throws Exception {
+        UsuarioResource usuarioResource = UsuarioResourceFixture.buildValido();
+
+        when(usuarioService.delete(any(UUID.class))).thenReturn(usuarioResource);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/usuarios/{id}", UUID.randomUUID()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(usuarioResource.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value(usuarioResource.getNome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sobrenome").value(usuarioResource.getSobrenome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value(usuarioResource.getCpf()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.papel").value(usuarioResource.getPapel().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dataNascimento").value(usuarioResource.getDataNascimento().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(usuarioResource.getEmail()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.celular").value(usuarioResource.getCelular()));
     }
 
 }
